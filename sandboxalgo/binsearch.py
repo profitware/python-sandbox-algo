961e5bd18a3d3bb8be8ac1b8f1283f5e676a2428
profitware <S.Sobko@profitware.ru>
2015-03-03 01:03:18 +0300

    Binary search

---
+++ sandboxalgo/binsearch.py
@@ -0,0 +1,93 @@
+# -*- coding: utf-8 -*-
+
+__author__ = 'Sergey Sobko'
+
+
+KEY_NOT_FOUND = -1
+
+
+def midpoint(imin, imax):
+    """Returns middle point
+
+    >>> midpoint(0, 0)
+    0
+
+    >>> midpoint(0, 1)
+    0
+
+    >>> midpoint(0, 2)
+    1
+
+    >>> midpoint(1, 1)
+    1
+
+    >>> midpoint(1, 2)
+    1
+
+    >>> midpoint(1, 5)
+    3
+
+    """
+
+    middle_point = (int(imin) + int(imax)) / 2
+    return middle_point
+
+
+def binary_search(search_list, key):
+    """Binary search algorithm
+
+    >>> binary_search([], 1) == KEY_NOT_FOUND
+    True
+
+    >>> binary_search([1, 3, 4, 6, 8, 9, 11], 4)
+    2
+
+    >>> binary_search([1, 2, 3, 4, 6, 8, 9, 11], 4)
+    3
+
+    >>> binary_search([1, 2, 3, 4, 6, 8, 9, 11], 1)
+    0
+
+    >>> binary_search([1, 2, 3, 4, 6, 8, 9, 11], 11)
+    7
+
+    >>> binary_search([1, 2, 3, 4, 8, 9, 11], 11)
+    6
+
+    >>> binary_search([1, 2, 3], 4) == KEY_NOT_FOUND
+    True
+
+    >>> binary_search([-1, 2, 4], 0) == KEY_NOT_FOUND
+    True
+
+
+    """
+
+    if not search_list:
+        return KEY_NOT_FOUND
+
+    list_last_index = len(search_list) - 1
+
+    if search_list[list_last_index] == key:
+        return list_last_index
+
+    def _binary_search(imin, imax, previous_index=-1):
+        current_index = midpoint(imin, imax)
+
+        if previous_index == current_index:
+            return KEY_NOT_FOUND
+
+        if search_list[current_index] == key:
+            return current_index
+
+        if key < search_list[current_index]:
+            return _binary_search(imin, current_index, current_index)
+        else:
+            return _binary_search(current_index, imax, current_index)
+
+    return _binary_search(0, list_last_index)
+
+
+if __name__ == '__main__':
+    import doctest
+    doctest.testmod()
